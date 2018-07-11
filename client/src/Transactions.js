40fa8dfa3255c22ae2b8afa555977ac154b51a55
Patrick Shaw <sixtey7@gmail.com>
2018-07-11 10:08:42 -0400

    start adding editable cells in the table

--- client/src/Transactions.js
+++ client/src/Transactions.js
@@ -5,6 +5,11 @@ import 'react-table/react-table.css';
 class Transaction extends Component {
     state = {transactions: []}
 
+    constructor() {
+        super();
+        this.renderEditable = this.renderEditable.bind(this);
+    }
+
     componentDidMount() {
         //get the transaction data from the backend
         fetch('/transactions')
@@ -13,6 +18,33 @@ class Transaction extends Component {
                 return a.id - b.id
             }))
             .then(transactions => this.setState({ transactions }));
+    }
+
+    renderEditable(cellInfo) {
+        return (
+            <div 
+                style={{ backgroundColor: '#fafafa' }}
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={ e => {
+                    console.log('on blur running!');
+                    const transaction = this.state.transactions[cellInfo.index];
+                    transaction['data'][cellInfo.column.id] = e.target.innerHTML;
+
+                    console.log('Set the property to: ' + JSON.stringify(transaction));
+                    //TODO make a post request to the backend to make the update
+
+                    //TODO if a debit or credit or date amount changes, need to do stuff
+                }}
+                dangerouslySetInnerHTML={{
+                    __html: this.state.transactions[cellInfo.index]['data'][cellInfo.column.id]
+                }}
+            />
+        );
+    }
+
+    renderAmountEditable(cellInfo) {
+        //TODO Gotta handle the case where the debit or credit cells are edited, those need special attention
     }
 
     render() {
@@ -26,7 +58,8 @@ class Transaction extends Component {
                             columns: [
                                 {
                                     Header: 'Name',
-                                    accessor: 'data.name'
+                                    accessor: 'name',
+                                    Cell: this.renderEditable
                                 }
                             ]
                         },
@@ -74,7 +107,8 @@ class Transaction extends Component {
                                 },
                                 {
                                     Header: 'Notes',
-                                    accessor: 'data.notes'
+                                    accessor: 'notes',
+                                    Cell: this.renderEditable
                                 }
                             ]
                         }
