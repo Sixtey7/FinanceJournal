c4c7ee3b688309781390214b6d1b6970ef481e7e
Patrick Shaw <sixtey7@gmail.com>
2018-07-11 09:25:26 -0400

    use react table to build a decent looking table

--- client/src/Transactions.js
+++ client/src/Transactions.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 class Transaction extends Component {
     state = {transactions: []}
@@ -7,29 +9,80 @@ class Transaction extends Component {
         //get the transaction data from the backend
         fetch('/transactions')
             .then(res => res.json())
+            .then(transactions => transactions.sort(function(a, b){ 
+                return a.id - b.id
+            }))
             .then(transactions => this.setState({ transactions }));
     }
 
     render() {
         return (
-            <div className="TransactionTable">
-                <h1>Transactions</h1>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Date</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.transactions.map(transaction => 
-                        <tr>
-                            <td>{transaction.data.name}</td>
-                            <td>{transaction.data.date}</td>
-                        </tr>
-                    )}
-                    </tbody>
-                </table>
+            <div>
+                <ReactTable
+                    data= { this.state.transactions }
+                    columns = {[
+                        {
+                            Header: 'Source',
+                            columns: [
+                                {
+                                    Header: 'Name',
+                                    accessor: 'data.name'
+                                }
+                            ]
+                        },
+                        {
+                            Header: 'Finances',
+                            columns: [
+                                {
+                                    Header: 'Debit',
+                                    accessor: 'data.amount',
+                                    Cell: row => (
+                                        <span>
+                                            {
+                                                row.value < 0 ? Math.abs(row.value) : ''
+                                            }
+                                        </span>
+                                    )
+                                },
+                                {
+                                    Header: 'Credit',
+                                    accessor: 'data.amount',
+                                    Cell: row => (
+                                        <span>
+                                            {
+                                                row.value > 0 ? row.value : ''
+                                            }
+                                        </span>
+                                    )
+                                },
+                                {
+                                    Header: 'Total',
+                                    accessor: 'total'
+                                }
+                            ]
+                        },
+                        {
+                            Header: 'Details',
+                            columns: [
+                                {
+                                    Header: 'Date',
+                                    accessor: 'data.date'
+                                },
+                                {
+                                    Header: 'Type',
+                                    accessor: 'data.type'
+                                },
+                                {
+                                    Header: 'Notes',
+                                    accessor: 'data.notes'
+                                }
+                            ]
+                        }
+
+                    ]}
+                    defaultPageSize={10}
+                    className="-striped -highlight"
+                />
             </div>
         );
     }
