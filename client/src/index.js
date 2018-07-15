6fba0511593bfde020dccdcab99677b38ec4483b
Patrick Shaw <sixtey7@gmail.com>
2018-07-14 23:25:33 -0400

    remove the old app stuff from index

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Transaction from './Transactions';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Transaction />, document.getElementById('root'));
 registerServiceWorker();
